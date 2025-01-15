d80fa936998ee37510f60937254cbac0b317d653
4th-avenue <quebec706@gmail.com>
2025-01-15 20:48:24 +0900

    모바일 화면의 header 디자인

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,11 +1,28 @@
-<div class="w-full">
+<div class="w-full h-full">
     {{-- Header --}}
-    <header>
+    <header class="md:hidden sticky top-0 bg-white">
+        <div class="grid grid-cols-12 gap-2 items-center">
+            <div class="col-span-3">
+                <img src="{{ asset('assets/logo.png') }}" alt="logo" class="h-12 max-w-lg w-full">
+            </div>
+            <div class="col-span-8 flex justify-center px-2">
+                <input type="text" placeholder="Search" class="border-0 outline-none w-full focus:outline-none bg-gray-100 rounded-lg focus:ring-0 hover:ring-0">
+            </div>
+            <div class="col-span-1 flex justify-center">
+                <a href="#">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                          </svg>                          
+                    </span>
+                </a>
+            </div>
+        </div>
     </header>
 
     {{-- main --}}
     <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
-        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+        <aside class="lg:col-span-8 overflow-hidden">
             {{-- Stories --}}
             <section>
                 <ul class="flex overflow-x-auto scrollbar-hide items-center gap-2">
@@ -19,7 +36,7 @@
             </section>
         </aside>
 
-        <aside class="lg:col-span-4 border hidden lg:block p-4">
+        <aside class="lg:col-span-4 hidden lg:block p-4">
             <div class="flex items-center gap-2">
                 <x-avatar src="https://source.unsplash.com/500x500?face" class="h-12 w-12" />
                 <h4 class="font-medium">{{ fake()->name }}</h4>
@@ -30,8 +47,9 @@
                 <h4 class="font-bold text-gray-700/95">Suggestions for you</h4>
 
                 <ul class="my-2 space-y-3">
+                    @for ($i = 0; $i < 5; $i++)
                     <li class="flex items-center gap-3">
-                        <x-avatar src="https://source.unsplash.com/500x500?face" class="h-12 w-12" />
+                        <x-avatar src="https://source.unsplash.com/500x500?face-{{$i}}" class="h-12 w-12" />
 
                         <div class="grid grid-cols-7 w-full gap-2">
                             <div class="col-span-5">
@@ -44,6 +62,7 @@
                             </div>
                         </div>
                     </li>
+                    @endfor
                 </ul>
             </section>
 
