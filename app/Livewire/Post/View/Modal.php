dedb5e53f1e921450f065841d0ecd25daba4f5cf
4th-avenue <quebec706@gmail.com>
2025-01-24 20:28:14 +0900

    Post/View/Modal 컴포넌트 기능 및 UI 개선

--- app/Livewire/Post/View/Modal.php
+++ app/Livewire/Post/View/Modal.php
@@ -2,16 +2,44 @@
 
 namespace App\Livewire\Post\View;
 
-use Livewire\Component;
+use App\Models\Post;
+use LivewireUI\Modal\ModalComponent;
 
-class Modal extends Component
+class Modal extends ModalComponent
 {
+    public $post;
+
+    public static function modalMaxWidth(): string
+    {
+        return '5xl';
+    }
+
+    public static function closeModalOnEscape(): bool
+    {
+        return false;
+    }
+
+    public function mount()
+    {
+        $this->post= Post::findOrFail($this->post);
+
+        // get url
+        $url= url('post/'.$this->post->id);
+
+        // push state using new livewwire v3 js helper
+        $this->js("history.pushState({},'','{$url}')");
+    }
+
     public function render()
     {
-        return <<<'HTML'
-        <div>
-            {{-- The best athlete wants his opponent at his best. --}}
-        </div>
-        HTML;
+        return <<<'BLADE'
+        <main class="bg-white h-[calc(100vh_-_3.5rem)] md:h-[calc(100vh_-_5rem)] flex flex-col border gap-y-4 px-5">
+            <header class="w-full py-2">
+                <div class="flex justify-end">
+                    <button wire:click="$dispatch('closeModal')" type="button" class="xl font-bold">X</button>
+                </div>
+            </header>
+        </main>
+        BLADE;
     }
 }
