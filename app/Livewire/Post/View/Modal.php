1be9fa27bf2a2c0c22bad8ad3f3dee2fa141cf99
4th-avenue <quebec706@gmail.com>
2025-01-24 14:48:40 +0900

    LIVEWIRE Post/View/Modal 컴포넌트 생성 (--inline)

---
+++ app/Livewire/Post/View/Modal.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Post\View;
+
+use Livewire\Component;
+
+class Modal extends Component
+{
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            {{-- The best athlete wants his opponent at his best. --}}
+        </div>
+        HTML;
+    }
+}
