f7644e9766c00058fcd400fec09d9d7baa6a96d1
4th-avenue <quebec706@gmail.com>
2025-01-26 17:10:34 +0900

    Post 모델과 Comment 모델과 User 모델의 릴레이션 설정

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,11 +2,37 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    // parent 
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Self::class, 'parent_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id', 'id')->with('replies');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
