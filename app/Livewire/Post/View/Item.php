d430b5f0e358362ad538d361de83f68315744923
4th-avenue <quebec706@gmail.com>
2025-01-30 13:27:17 +0900

    생성된 가상의 댓글 데이터 표시하기

--- app/Livewire/Post/View/Item.php
+++ app/Livewire/Post/View/Item.php
@@ -11,6 +11,10 @@ class Item extends Component
 
     public function render()
     {
-        return view('livewire.post.view.item');
+        $comments = $this->post->comments;
+
+        return view('livewire.post.view.item', [
+            'comments' => $comments
+        ]);
     }
 }
