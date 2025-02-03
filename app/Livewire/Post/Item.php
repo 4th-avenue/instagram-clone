9f89f95aedd412232ebc576e3f86e88cb40f6d95
4th-avenue <quebec706@gmail.com>
2025-02-03 15:09:30 +0900

    게시물 목록 페이지에서 댓글 저장 기능 구현 및 내가 쓴 댓글 표시하기

--- app/Livewire/Post/Item.php
+++ app/Livewire/Post/Item.php
@@ -2,11 +2,32 @@
 
 namespace App\Livewire\Post;
 
+use App\Models\Post;
+use App\Models\Comment;
 use Livewire\Component;
 
 class Item extends Component
 {
     public $post;
+
+    public $body;
+
+    public function addComment()
+    {
+        $this->validate([
+            'body' => 'required'
+        ]);
+
+        // create comment
+        Comment::create([
+            'body' => $this->body,
+            'commentable_id' => $this->post->id,
+            'commentable_type' => Post::class,
+            'user_id' => auth()->id(),
+        ]);
+
+        $this->reset('body');
+    }
 
     public function render()
     {
