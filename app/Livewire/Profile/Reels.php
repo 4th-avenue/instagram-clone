361d8b3cc79155614a2da4ee8763d54ded9282ac
4th-avenue <quebec706@gmail.com>
2025-02-12 14:13:57 +0900

    LIVEWIRE Profile/Home 컴포넌트를 복사해서 Profile/Reels와 Profile/Saved 컴포넌트 생성

---
+++ app/Livewire/Profile/Reels.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire\Profile;
+
+use App\Models\User;
+use Livewire\Component;
+use Livewire\Attributes\On;
+
+class Reels extends Component
+{
+    public $user;
+
+    #[On('closeModal')]
+    public function reverseUrl()
+    {
+        $this->js("history.replaceState({}, '', '/')");
+    }
+
+    function toggleFollow()
+    {
+        abort_unless(auth()->check(), 401);
+        auth()->user()->toggleFollow($this->user);
+    }
+
+    public function mount($user)
+    {
+        $this->user = User::whereUsername($user)->withCount(['followers', 'followings', 'posts'])->firstOrFail();
+    }
+
+    public function render()
+    {
+        $this->user = User::whereUsername($this->user->username)->withCount(['followers', 'followings', 'posts'])->firstOrFail();
+
+        $posts = $this->user->posts()->where('type', 'post')->get();
+
+        return view('livewire.profile.reels', [
+            'posts' => $posts
+        ])->layout('layouts.app');
+    }
+}
