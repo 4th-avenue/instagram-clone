c59dcb9ea606a89f6f9fc6b76f2b91f331dbdd3b
4th-avenue <quebec706@gmail.com>
2025-02-13 14:35:20 +0900

    LIVEWIRE Explore 컴포넌트 생성하고 Route 및 메뉴(내비게이션) 수정

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
 use App\Livewire\Home;
+use App\Livewire\Explore;
 use App\Livewire\Profile\Reels;
 use App\Livewire\Profile\Saved;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Profile\Home as ProfileHome;
 
-Route::get('/', Home::class)
-    ->middleware('auth')
-    ->name('/');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', Home::class)->name('/');
+    Route::get('/explore', Explore::class)->name('explore');
+
     // Route::view('profile', 'profile'->name('profile');
     Route::get('profile/{user}', ProfileHome::class)->name('profile.home');
     Route::get('profile/{user}/reels', Reels::class)->name('profile.reels');
