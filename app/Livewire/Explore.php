e577aff1e061273cbf79f2817032468b67212f89
4th-avenue <quebec706@gmail.com>
2025-02-13 19:46:00 +0900

    explore 페이지 디자인 및 게시물 데이터 표시하기 (group-hover:flex)

--- app/Livewire/Explore.php
+++ app/Livewire/Explore.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class Explore extends Component
 {
+    #[On('closeModal')]
+    public function reverseUrl()
+    {
+        $this->js("history.replaceState({}, '', '/explore')");
+    }
+
     public function render()
     {
-        return view('livewire.explore')->layout('layouts.app');
+        $posts = Post::limit(20)->get();
+
+        return view('livewire.explore', [
+            'posts' => $posts
+        ])->layout('layouts.app');
     }
 }
