e78d186424122e21da9cc1ce01fb0a5fab187b4f
4th-avenue <quebec706@gmail.com>
2025-02-14 16:52:43 +0900

    LIVEWIRE Reels 컴포넌트 생성하고 Route 및 메뉴(내비게이션) 수정

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Livewire\Home;
+use App\Livewire\Reels;
 use App\Livewire\Explore;
-use App\Livewire\Profile\Reels;
 use App\Livewire\Profile\Saved;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Profile\Home as ProfileHome;
+use App\Livewire\Profile\Reels as ProfileReels;
 
 // Route::view('dashboard', 'dashboard')
 //     ->middleware(['auth', 'verified'])
@@ -13,11 +14,12 @@ use App\Livewire\Profile\Home as ProfileHome;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', Home::class)->name('/');
-    Route::get('/explore', Explore::class)->name('explore');
+    Route::get('explore', Explore::class)->name('explore');
+    Route::get('reels', Reels::class)->name('reels');
 
     // Route::view('profile', 'profile'->name('profile');
     Route::get('profile/{user}', ProfileHome::class)->name('profile.home');
-    Route::get('profile/{user}/reels', Reels::class)->name('profile.reels');
+    Route::get('profile/{user}/reels', ProfileReels::class)->name('profile.reels');
     Route::get('profile/{user}/saved', Saved::class)->name('profile.saved');
 });
 
