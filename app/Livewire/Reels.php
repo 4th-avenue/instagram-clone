34fc647a6024c33cb8c030d4119100cf996e6fdb
4th-avenue <quebec706@gmail.com>
2025-02-15 17:00:29 +0900

    reels 페이지 디자인 및 좋아요 기능과 저장 기능 추가

--- app/Livewire/Reels.php
+++ app/Livewire/Reels.php
@@ -2,12 +2,38 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class Reels extends Component
 {
+    #[On('closeModal')]
+    public function reverseUrl()
+    {
+        $this->js("history.replaceState({}, '', '/reels')");
+    }
+
+    public function togglePostLike(Post $post)
+    {
+        abort_unless(auth()->check(), 401);
+
+        auth()->user()->toggleLike($post);
+    }
+
+    public function toggleFavorite(Post $post)
+    {
+        abort_unless(auth()->check(), 401);
+
+        auth()->user()->toggleFavorite($post);        
+    }
+
     public function render()
     {
-        return view('livewire.reels')->layout('layouts.app');
+        $posts = Post::limit(20)->where('type', 'reel')->get();
+
+        return view('livewire.reels', [
+            'posts' => $posts
+        ])->layout('layouts.app');
     }
 }
