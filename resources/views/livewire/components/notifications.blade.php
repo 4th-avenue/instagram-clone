36fdde490c065515f55bd31f0fc2ff7289382916
4th-avenue <quebec706@gmail.com>
2025-03-17 21:25:02 +0900

    새 팔로워 알림 표시하기

--- resources/views/livewire/components/notifications.blade.php
+++ resources/views/livewire/components/notifications.blade.php
@@ -4,64 +4,79 @@
 
     <main class="my-7 w-full">
         <div class="space-y-5">
-            {{-- NewFollower --}}
-            <div class="grid grid-cols-12 gap-2 w-full">
-                <a href="#" class="col-span-2">
-                    <x-avatar wire:ignore src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="h-10 w-10" />
-                </a>
+            @foreach ($notifications as $notification)
+            @switch($notification->type)
+                @case('App\Notifications\NewFollowerNotification')
+                    @php
+                        $user = \App\Models\User::find($notification->data['user_id']);
+                    @endphp
+                    {{-- NewFollower --}}
+                    <div class="grid grid-cols-12 gap-2 w-full">
+                        <a href="{{ route('profile.home', $user->username) }}" class="col-span-2">
+                            <x-avatar wire:ignore src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="h-10 w-10" />
+                        </a>
 
-                <div class="col-span-7 font-medium">
-                    <a href="#"><strong>{{ fake()->name }}</strong></a>
-                    Started Following you
-                    <span class="text-gray-400">2d</span>
-                </div>
+                        <div class="col-span-7 font-medium">
+                            <a href="{{ route('profile.home', $user->username) }}"><strong>{{ $user->name }}</strong></a>
+                            Started Following you
+                            <span class="text-gray-400">{{ $notification->created_at->shortAbsoluteDiffForHumans() }}</span>
+                        </div>
 
-                <div class="col-span-3">
-                    <button class="font-bold text-sm bg-blue-500 text-white px-3 py-1 rounded-lg">Follow</button>
-                    {{-- <button class="font-bold text-sm bg-gray-100 text-black/90 px-3 py-1 rounded-lg">Following</button> --}}
-                </div>
-            </div>
+                        <div class="col-span-3">
+                            @if (auth()->user()->isFollowing($user))
+                                <button wire:click="toggleFollow({{ $user->id }})" class="font-bold text-sm bg-gray-100 text-black/90 px-3 py-1 rounded-lg">Following</button>
+                            @else
+                                <button wire:click="toggleFollow({{ $user->id }})" class="font-bold text-sm bg-blue-500 text-white px-3 py-1 rounded-lg">Follow</button>
+                            @endif
+                        </div>
+                    </div>
+                    @break
+                @case('App\Notifications\PostLikedNotification')
+                    {{-- PostLiked --}}
+                    <div class="grid grid-cols-12 gap-2 w-full">
+                        <a href="#" class="col-span-2">
+                            <x-avatar wire:ignore src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="h-10 w-10" />
+                        </a>
 
-            {{-- PostLiked --}}
-            <div class="grid grid-cols-12 gap-2 w-full">
-                <a href="#" class="col-span-2">
-                    <x-avatar wire:ignore src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="h-10 w-10" />
-                </a>
+                        <div class="col-span-7 font-medium">
+                            <a href="#"><strong>{{ fake()->name }}</strong></a>
 
-                <div class="col-span-7 font-medium">
-                    <a href="#"><strong>{{ fake()->name }}</strong></a>
+                            <a href="#">
+                                Liked your post
+                                <span class="text-gray-400">2d</span>
+                            </a>
+                        </div>
 
-                    <a href="#">
-                        Liked your post
-                        <span class="text-gray-400">2d</span>
-                    </a>
-                </div>
+                        <a href="#" class="col-span-3 ml-auto">
+                            <img src="https://source.unsplash.com/500x500?nature-{{rand(0,10)}}" alt="image" class="h-11 w-10 object-cover">
+                        </a>
+                    </div>
+                    @break
+                @case('App\Notifications\NewCommentNotification')
+                    {{-- NewComment --}}
+                    <div class="grid grid-cols-12 gap-2 w-full">
+                        <a href="#" class="col-span-2">
+                            <x-avatar wire:ignore src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="h-10 w-10" />
+                        </a>
 
-                <a href="#" class="col-span-3 ml-auto">
-                    <img src="https://source.unsplash.com/500x500?nature-{{rand(0,10)}}" alt="image" class="h-11 w-10 object-cover">
-                </a>
-            </div>
+                        <div class="col-span-7 font-medium">
+                            <a href="#"><strong>{{ fake()->name }}</strong></a>
 
-            {{-- NewComment --}}
-            <div class="grid grid-cols-12 gap-2 w-full">
-                <a href="#" class="col-span-2">
-                    <x-avatar wire:ignore src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="h-10 w-10" />
-                </a>
+                            <a href="#">
+                                Commented:
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                <span class="text-gray-400">2d</span>
+                            </a>
+                        </div>
 
-                <div class="col-span-7 font-medium">
-                    <a href="#"><strong>{{ fake()->name }}</strong></a>
-
-                    <a href="#">
-                        Commented:
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        <span class="text-gray-400">2d</span>
-                    </a>
-                </div>
-
-                <a href="#" class="col-span-3 ml-auto">
-                    <img src="https://source.unsplash.com/500x500?nature-{{rand(0,10)}}" alt="image" class="h-11 w-10 object-cover">
-                </a>
-            </div>
+                        <a href="#" class="col-span-3 ml-auto">
+                            <img src="https://source.unsplash.com/500x500?nature-{{rand(0,10)}}" alt="image" class="h-11 w-10 object-cover">
+                        </a>
+                    </div>
+                    @break
+                @default
+            @endswitch
+            @endforeach
         </div>
     </main>
 </div>
